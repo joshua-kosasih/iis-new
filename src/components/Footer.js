8ad281f66f57ae4faca6c217de5f41f434fd9720
Joshua Kosasih <kosasihjoshua5@gmail.com>
2022-08-15 14:26:34 +0700

    Fist Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './Footer.css';
+
+const Footer = () => (
+  <div className="footer">
+    <p>Copyright &#169; IPEKA 2022</p>
+  </div>
+);
+
+export default Footer;
---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+  {
+    title: 'Puri',
+    path: '/marketing',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'BSD',
+    path: '/consulting',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Grand Wisata',
+    path: '/design',
+    cName: 'dropdown-link'
+  },
+  {
+    title: 'Balikpapan',
+    path: '/development',
+    cName: 'dropdown-link'
+  }
+];
